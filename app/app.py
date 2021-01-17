903a9922388d307d0fb65a9514a25cd326238e61
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-01-18 01:26:34 +0900

    検索機能

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -21,6 +21,32 @@ def team(logo):
     contents = uniformNumber.query.filter_by(team=logo).all()
     print(contents)
     return render_template("team.html", contents=contents)
+
+@app.route('/search', methods=["POST"])
+def search():
+    team = request.form["team"]
+    num = request.form["number"]
+    print("{}, {}".format(team, num))
+    content = uniformNumber.query.filter_by(num=num, team=team).all()
+    print(content)
+    if content == []:
+        print('なかったね')
+        return redirect(url_for("result", isExisted=False,num=num, team=team))
+    else:
+        return redirect(url_for("result", isExisted=True, history=content[0].history,num=num, team=team))
+
+@app.route('/result')
+def result():
+    isExisted = request.args.get('isExisted')
+    num=request.args.get('num')
+    history=request.args.get('history')
+    team=request.args.get('team')
+    print(history)
+    if (isExisted):
+        return render_template("result.html", history=history, num=num,team=team)
+    else:
+        return render_template("result.html",num=num,team=team)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
