23bf885ebf2714c8b95765b36882845c845609c2
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-01-19 13:09:25 +0900

    420error回避

--- app/app.py
+++ app/app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 from app.models import uniformNumber
-from app.check import start
 
 @app.route('/')
 def index():
