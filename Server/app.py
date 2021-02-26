75f638a047ed2a6c7f229af1301c3f33e5ff77f1
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-02-26 18:19:38 +0900

    tableの参照先を変更, ユーザー追加機能

--- Server/app.py
+++ Server/app.py
@@ -17,10 +17,20 @@ def get_all(query):
     finally:
         con.close()
 
+
+def insert(query):
+    con = engine.connect()
+    try:
+        return con.execute(query)
+    finally:
+        con.close()
+
+
 def convertToRes(arr):
     response = []
     for i in range(len(arr)):
-        response.append({'teamname': arr[i].teamname, 'num': arr[i].num, 'history': arr[i].history, 'id': arr[i].id})
+        response.append(
+            {'teamname': arr[i].teamname, 'num': arr[i].num, 'history': arr[i].history, 'id': arr[i].id})
     return response
 
 
@@ -28,31 +38,47 @@ def convertToRes(arr):
 def searchByTeam():
     team = request.args.get('team')
     print('GET searchByTeam ', team)
-    query = "select * from allteam2020 where teamname='{}' order by id ASC;".format(
+    query = "select * from info where teamname='{}' order by id ASC;".format(
         team)
     result = get_all(query)
     response = convertToRes(result)
     return jsonify({'data': response})
 
+
 @app.route('/api/num', methods=["GET"])
 def searchByNum():
     num = request.args.get('num')
     print('GET searchByNum ', num)
-    query = "select * from allteam2020 where num='{}' order by id ASC;".format(
+    query = "select * from info where num='{}' order by id ASC;".format(
         num)
     result = get_all(query)
     response = convertToRes(result)
     return jsonify({'data': response})
 
+
 @app.route('/api/keyword', methods=["GET"])
 def searchByKeyword():
     key = request.args.get('keyword')
     print('GET searchByKeyword ', key)
-    query = "select * from allteam2020 where history like '%%{}%%' order by id ASC;".format(
+    query = "select * from info where history like '%%{}%%' order by id ASC;".format(
         key)
     result = get_all(query)
     response = convertToRes(result)
     return jsonify({'data': response})
+
+
+@app.route('/api/register/user', methods=["POST"])
+def registerUser():
+    payload = request.json
+    currentq = "select * from userlist"
+    res = get_all(currentq)
+    id = len(res) + 1
+    print({'id': id, 'uid': payload.get('uid'), 'email': payload.get(
+        'email'), 'name': payload.get('name')})
+    query = "insert into userlist values({}, '{}', '{}', '{}')".format(
+        id, payload.get('uid'), payload.get('email'), payload.get('name'))
+    insert(query)
+    return jsonify({'data': 1})
 
 
 if __name__ == "__main__":
