dfacf771a1ce85aa02d4b9e22306a49d01aee81e
manattan <mogimogi0529@eis.hokudai.ac.jp>
2021-03-18 18:14:47 +0900

    fix config

--- Server/app.py
+++ Server/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
 from sqlalchemy import create_engine
 from sqlalchemy.pool import QueuePool
-import config
+import Server.config
 
 app = Flask(__name__)
 CORS(app, support_credentials=True)
